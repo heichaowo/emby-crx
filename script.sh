c3f3302dddaabb795a95e8f5e81e57dda198be09
heichaowo <22045978+heichaowo@users.noreply.github.com>
2026-03-24 18:00:58 +0800

    chore: cleanup redundant resources, update install script
    - Removed res/ directory (old icon.png + icon.psd)
    - Added .gitignore for .DS_Store
    - Updated script.sh with new repo URLs and branch name
    - Fixed variable name reference in README

--- script.sh
+++ script.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# 创建emby-crx目录并下载所需文件
-rm -rf emby-crx
-mkdir -p emby-crx
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/css/style.css -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/common-utils.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/md5.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/content/main.js -P emby-crx/
+# Emby Beautify 服务端安装脚本
+# 在 Emby 的 web 目录下执行此脚本
 
-# 读取index.html文件内容
-content=$(cat index.html)
+rm -rf emby-beautify
+mkdir -p emby-beautify
+wget https://raw.githubusercontent.com/heichaowo/emby-beautify/main/static/css/style.css -P emby-beautify/
+wget https://raw.githubusercontent.com/heichaowo/emby-beautify/main/static/js/common-utils.js -P emby-beautify/
+wget https://raw.githubusercontent.com/heichaowo/emby-beautify/main/static/js/jquery-3.6.0.min.js -P emby-beautify/
+wget https://raw.githubusercontent.com/heichaowo/emby-beautify/main/static/js/md5.min.js -P emby-beautify/
+wget https://raw.githubusercontent.com/heichaowo/emby-beautify/main/content/main.js -P emby-beautify/
 
-# 检查index.html是否包含emby-crx
-if grep -q "emby-crx" index.html; then
-    echo "Index.html already contains emby-crx, skipping insertion."
+# 检查 index.html 是否已注入
+if grep -q "emby-beautify" index.html; then
+    echo "Index.html already contains emby-beautify, skipping insertion."
 else
     # 定义要插入的代码
-    code='<link rel="stylesheet" id="theme-css" href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/main.js"></script>'
+    code='<link rel="stylesheet" id="theme-css" href="emby-beautify/style.css" type="text/css" media="all" />\n<script src="emby-beautify/common-utils.js"></script>\n<script src="emby-beautify/jquery-3.6.0.min.js"></script>\n<script src="emby-beautify/md5.min.js"></script>\n<script src="emby-beautify/main.js"></script>'
 
-    # 在</head>之前插入代码
+    # 在 </head> 之前插入代码
     new_content=$(echo -e "${content/<\/head>/$code<\/head>}")
 
-    # 将新内容写入index.html文件
+    # 将新内容写入 index.html
     echo -e "$new_content" > index.html
+    echo "Emby Beautify injected successfully."
 fi
